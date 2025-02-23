99b8065f7311123bac69d163d65e2a116f9c7fe6
Nazariman <NazarBor.iii@gmail.com>
2025-02-23 03:05:20 +0200

    Create class cart and main.py

---
+++ HW1/Class_Cart.py
@@ -0,0 +1,41 @@
+'''
+Створіть клас Cart(кошик клієнта магазину) з атрибутами
+client(ім’я клієнта) та items(список товарів).
+Додайте метод який додає новий товар до кошика
+Додайте метод який видаляє товар з кошика
+Додайте метод для виведення інформації про кошик
+'''
+
+class Cart: 
+    def __init__ (self, client): 
+        self.client = client
+        self.items = []
+        
+    def add_item(self, item): 
+        self.items.append(item)
+        return f"Товар {item} було додано до кошика"
+        
+    def remove_item(self, item): 
+            if item in self.items: 
+                self.items.remove(item)
+                return f"{item} було видалено з списку"
+            else: 
+                return f"{item} не знайдено в списку"
+    
+    def show_cart(self):
+        print(f"Кошик клієнта: {self.client}")
+        if self.items:
+            print("Товари у кошику:")
+            for item in self.items:
+                print(f" - {item}")
+        else:
+            return "Кошик порожній."
+        
+        
+        
+cart = Cart("Олександр")
+cart.add_item("Ноутбук")
+cart.add_item("Смартфон")
+cart.show_cart()
+cart.remove_item("Ноутбук")
+cart.show_cart()
